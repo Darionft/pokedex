3e73cf6b21aaff3dd3fe3dc55141e495003625b4
Darionft <d.acew@hotmail.com>
2023-05-03 21:51:39 -0300

    deploy js
    Começando o JS

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () =>{
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+        
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        console.log(document.getElementById('cartao-bulbasaur'))
+    })
+})
